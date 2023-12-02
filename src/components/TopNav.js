ad4b2fe235428dfc964b5d3ce06a0dc3c51ae3a1
ayeshasikander <sikanderayesha46@gmail.com>
2023-12-02 12:19:51 +0500

    authentication complete

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,81 +1,113 @@
-import React from 'react';
+import React from "react";
 import { AiOutlineLogout } from "react-icons/ai";
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import { onAuthStateChanged,signOut } from 'firebase/auth';
+import { firebaseAuth } from "../utils/firebase-config";
+
 
 const TopNav = ({ isScroll }) => {
-    const navlinks = [
-        { name: "Home", link: '/' },
-        { name: "TV Show", link: '/tv' },
-        { name: "My List", link: '/mylist' },
-        { name: "Movies", link: '/movies' },
-    ]
-    return (
-        <NavContainer isScroll={isScroll}>
-            <nav>
-                <div className="left-side">
-                    <div className="logo">
-                        <img src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668265433/NetflixApp/2560px-Netflix_2015_logo.svg_rbicwl_knwp6f.png" alt="Netflix" />
-                    </div>
-                    <ul className="links">
-                        {
-                            navlinks.map(({ name, link }) => {
-                                return (
-                                    <li key={name}>
-                                        <Link to={link}>{name}</Link>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className="right-side">
-                    <button>
-                        <AiOutlineLogout />
-                    </button>
-                </div>
-            </nav>
-        </NavContainer>
-    )
-}
+  const navlinks = [
+    { name: "Home", link: "/" },
+    { name: "TV Show", link: "/tv" },
+    { name: "My List", link: "/mylist" },
+    { name: "Movies", link: "/movies" },
+  ];
+
+  const navigate = useNavigate();
+
+  onAuthStateChanged(firebaseAuth, (cureentUser) => {
+    if (!cureentUser) navigate("/login");
+  });
+  return (
+    <NavContainer isScroll={isScroll}>
+      <nav>
+        <div className="left-side">
+          <div className="logo">
+            <img
+              src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668265433/NetflixApp/2560px-Netflix_2015_logo.svg_rbicwl_knwp6f.png"
+              alt="Netflix"
+            />
+          </div>
+          <ul className="links">
+            {navlinks.map(({ name, link }) => {
+              return (
+                <li key={name}>
+                  <Link to={link}>{name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="right-side">
+          <button onClick={()=>signOut(firebaseAuth)}>
+          <AiOutlineLogout />
+          </button>
+        </div>
+      </nav>
+    </NavContainer>
+  );
+};
 
 const NavContainer = styled.div`
-nav{
+  nav {
     display: flex;
     align-items: center;
     justify-content: space-between;
     position: sticky;
     top: 0;
-    height: 6rem;
+    height: 5rem;
     position: fixed;
     z-index: 2;
     width: 100%;
-    background-color: ${props => (props.isScroll ? "black" : "transparent")};
+    background-color: ${(props) => (props.isScroll ? "black" : "transparent")};
     transition: 0.5s ease-in-out;
-    .left-side{
+    .left-side {
+      display: flex;
+      align-items: center;
+      gap: 2rem;
+
+      .logo {
         display: flex;
         align-items: center;
-        gap: 2rem;
-    }
-    .logo{
-       display: flex;
-       align-items: center;
-       justify-content: center;
-    }
-    img{
+        justify-content: center;
+      }
+      img {
         height: 2rem;
         width: 10rem;
+      }
     }
-}
-.links{
-    display: flex;
-    li{
-        a{
-            color: #fff;
+    .links {
+      display: flex;
+      list-style: none;
+      gap: 3rem;
+      li {
+        a {
+          text-decoration: none;
+          color: #fff;
         }
+      }
     }
-}
- 
-`
+  }
+  .right-side {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-right: 1rem;
+    button {
+      background-color: red;
+      border: none;
+      border-radius: 10%;
+      padding: 0.4rem 0.6rem;
+      
+      color: #fff;
+      cursor: pointer;
+    }&:focus{
+        outline: none;
+    }svg{
+        font-size: 1.5rem;
+    }
+  }
+`;
 
-export default TopNav
+export default TopNav;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,25 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Header from '../components/Header';
-import Backgroundimg from '../components/Backgroundimg';
-import { firebaseAuth } from '../utils/firebase-config';
-import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+import Backgroundimg from "../components/Backgroundimg";
+import { firebaseAuth } from "../utils/firebase-config";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const handleLogin = async () => {
     try {
-      await signInWithEmailAndPassword(firebaseAuth, email, password)
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   onAuthStateChanged(firebaseAuth, (cureentUser) => {
-    if (cureentUser) navigate('/')
-  })
+    if (cureentUser) navigate("/");
+  });
   return (
     <Wrapper>
       <Backgroundimg />
@@ -33,78 +31,80 @@ const Login = () => {
               <h1>login</h1>
             </div>
             <div className="container">
-              <input type="text" placeholder='email'
+              <input
+                type="text"
+                placeholder="email"
                 onChange={(e) => setEmail(e.target.value)}
-                value={email} />
-              <input type="password" placeholder='password'
+                value={email}
+              />
+              <input
+                type="password"
+                placeholder="password"
                 onChange={(e) => setPassword(e.target.value)}
-                value={password} />
+                value={password}
+              />
               <button onClick={handleLogin}>Login</button>
             </div>
           </div>
         </div>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
-position: relative;
-.loginContent{
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100vw;
-  background-color: rgba(0, 0, 0, 0.6);
-  grid-template-columns: 15vh 85vh;
-  .form-wrapper{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 2rem;
-    height: 85vh;
-  }
-  .form{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 2rem;
-    background-color: rgba(0, 0, 0, 0.887);
-    height: 75vh;
-    padding: 2rem;
-    border-radius: 0.4rem;
-     color: #fff;
-  }
-  .container{
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-    input{
-      padding: 0.5rem 1rem;
-      width: 25rem;
-      height: 3.4rem;
-      border-radius: 0.4rem;
-      outline: none;
+  position: relative;
+  .loginContent {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0.6);
+    grid-template-columns: 15vh 85vh;
+    .form-wrapper {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+      height: 85vh;
     }
-    button{
-      cursor: pointer;
-      padding: 0.5rem;
-      background-color: #D91921;
-      border: none;
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+      background-color: rgba(0, 0, 0, 0.887);
+      height: 75vh;
+      padding: 2rem;
       border-radius: 0.4rem;
-      height: 3.1rem;
       color: #fff;
-      font-size: 1.05rem;
-      font-weight: bolder;
+    }
+    .container {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+      input {
+        padding: 0.5rem 1rem;
+        width: 25rem;
+        height: 3.4rem;
+        border-radius: 0.4rem;
+        outline: none;
+      }
+      button {
+        cursor: pointer;
+        padding: 0.5rem;
+        background-color: #d91921;
+        border: none;
+        border-radius: 0.4rem;
+        height: 3.1rem;
+        color: #fff;
+        font-size: 1.05rem;
+        font-weight: bolder;
+      }
     }
   }
-
-  
-
-}
-
-`
-export default Login
+`;
+export default Login;
