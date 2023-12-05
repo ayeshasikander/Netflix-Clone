6444c870a99f0691886738a243c3e6998e0be64c
ayeshasikander <sikanderayesha46@gmail.com>
2023-12-05 19:31:42 +0500

    API

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    movies: [],
+    generesLoaded: false,
+    genres: []
+}
+const NetflixSlice = createSlice({
+    name: 'Netflix',
+    initialState,
+    extraReducers: (builder) => { }
+})
+
+export const store=configureStore({
+    reducer:{
+        netflix:NetflixSlice.reducer
+    }
+})
