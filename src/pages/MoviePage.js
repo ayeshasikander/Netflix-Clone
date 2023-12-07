a0329f752d426c6ac578d9aa44f907caf776b949
ayeshasikander <sikanderayesha46@gmail.com>
2023-12-07 16:17:13 +0500

    add something

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from 'react';
+import Backgroundimg from '../components/Backgroundimg';
 const MoviePage = () => {
   return (
     <div>
-      moviepage
+      <Backgroundimg/>
     </div>
   )
 }
